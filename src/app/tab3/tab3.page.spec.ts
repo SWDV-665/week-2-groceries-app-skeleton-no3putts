5b4cf17d89f592f91b6fe74beef8e401c75e6754
Jerrie Pineda <jeremiah.pineda@stevenshenager.edu>
2020-10-31 22:17:20 -0600

    Initial Commit

--- src/app/tab3/tab3.page.spec.ts
+++ src/app/tab3/tab3.page.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { IonicModule } from '@ionic/angular';
-import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+import { AboutContainerComponentModule } from '../about-container/about-container.module';
 
 import { Tab3Page } from './tab3.page';
 
@@ -11,7 +11,7 @@ describe('Tab3Page', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [Tab3Page],
-      imports: [IonicModule.forRoot(), ExploreContainerComponentModule]
+      imports: [IonicModule.forRoot(), AboutContainerComponentModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(Tab3Page);
